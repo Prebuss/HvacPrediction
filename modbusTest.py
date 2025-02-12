f6f663ee5b9966b051cdce385a52f6a93404017e
Preben Forsland <prebenforsland@gmail.com>
2025-02-12 10:24:22 +0100

    Added weather data 02.10 and 02.11

--- modbusTest.py
+++ modbusTest.py
@@ -11,13 +11,18 @@ def main():
     instrument.serial.bytesize = 8
     instrument.serial.parity   = serial.PARITY_NONE
     instrument.serial.stopbits = 1
-    instrument.serial.timeout  = 1  # seconds
+    instrument.serial.timeout  = 10  # seconds
     instrument.mode = minimalmodbus.MODE_RTU
 
     try:
         # 3xXXXX => input registers => function code = 4 in minimalmodbus
+        componentName                = instrument.read_register(0,   0, 4)   # 3x0001 
         co2_sensor_data              = instrument.read_register(267, 0, 4)   # 3x0267
-        outdoor_temperature          = instrument.read_register(73,  0, 4)   # 3x0073
+        new_co2                      = instrument.read_register(266, 0, 4)   # 3x0267 But zero adjusted
+        nothing                      = instrument.read_register(62,  0, 4)   # 3x0062 Should be zero
+        SFP                          = instrument.read_register(63,  0, 4)   # 3x0063 Specific Fan Power
+        SA_temp                      = instrument.read_register(64,  0, 4)   # 3x0064 Supply Air temperature
+        outdoor_temperature          = instrument.read_register(73,  2, 4, True)   # 3x0073
         supply_air_flow_pressure     = instrument.read_register(30,  0, 4)   # 3x0030
         supply_air_flow              = instrument.read_register(6,   0, 4)   # 3x0006
         ahu_filter_pressure_level    = instrument.read_register(27,  0, 4)   # 3x0027
@@ -26,6 +31,7 @@ def main():
         cooling_regulation_level     = instrument.read_register(101, 0, 4)   # 3x0101
         duct_pressure                = instrument.read_register(8,   0, 4)   # 3x0008
         voc_level                    = instrument.read_register(266, 0, 4)   # 3x0266
+        voc_level2                   = instrument.read_register(265, 0, 4)   # 3x0266 But zero adjusted
         rhx_operation_level          = instrument.read_register(105, 0, 4)   # 3x0105
         rhx_efficiency               = instrument.read_register(106, 0, 4)   # 3x0106
         rhx_defrost_pressure_level   = instrument.read_register(107, 0, 4)   # 3x0107
@@ -36,8 +42,30 @@ def main():
         min_temperature_set_point    = instrument.read_register(136, 0, 3)   # 4x0136
         max_temperature_set_point    = instrument.read_register(137, 0, 3)   # 4x0137
 
+        # Test registers:
+        AppID                        = instrument.read_register(1,   0, 3)   # 3x0002
+        PHX23D                       = instrument.read_register(114, 0, 3)   # 3x0114
+        XzoneCoil                    = instrument.read_register(197, 0, 3)   # 3x0198
+        RhxOperationTime             = instrument.read_register(111, 0, 3)   # 3x0111 
+        #OperationLevel               = instrument.read_register(592, 0, 4)   # 3x0592 - This was illegal
+        #SoftwareVersion              = instrument.read_register(570, 0, 4)   # 3x0570
+
+        # Print test:
+        print("Application ID:                  ", AppID)
+        print("PHX-2 3D Type:                   ", PHX23D)
+        print("XZone Coil Type:                 ", XzoneCoil)
+        print("RHX Operation Time:              ", RhxOperationTime)
+        #print("Operation level:                 ", OperationLevel)
+        #print("Software version:                ", SoftwareVersion)
+        print("---------------------------------\n")
+
         # Print out results
+        print("Component name:                  ", componentName)
         print("CO? sensor data:                 ", co2_sensor_data)
+        print("Co2 sensor data:                 ", new_co2)
+        print("This should be zero:             ", nothing)
+        print("SFP:                             ", SFP)
+        print("Supply air temperature           ", SA_temp)
         print("Outdoor temperature:             ", outdoor_temperature)
         print("Supply air flow pressure:        ", supply_air_flow_pressure)
         print("Supply air flow:                 ", supply_air_flow)
@@ -47,6 +75,7 @@ def main():
         print("Cooling regulation level:        ", cooling_regulation_level)
         print("Duct pressure:                   ", duct_pressure)
         print("VOC level:                       ", voc_level)
+        print("VOC level zero adjusted:         ", voc_level2)
         print("RHX operation level:             ", rhx_operation_level)
         print("RHX efficiency:                  ", rhx_efficiency)
         print("RHX defrost pressure level:      ", rhx_defrost_pressure_level)
